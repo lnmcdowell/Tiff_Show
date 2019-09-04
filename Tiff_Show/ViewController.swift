52005cb8295c57d2a9a53614196b6c1325028f19
nathanielmcdowell <nathaniel29651@gmail.com>
2019-09-04 14:41:49 -0400

    working demo of loading an image - however, geotiff is impracticably large. jpg from extraction using QGIS is loaded here - still barely works in simulator due to immense size.

--- Tiff_Show/ViewController.swift
+++ Tiff_Show/ViewController.swift
@@ -7,13 +7,76 @@
 //
 
 import UIKit
+import tiff_ios
+
 
 class ViewController: UIViewController {
+  //  var filePath = Bundle.main.path(forResource: "Atlanta", ofType: "tif")
+    let file: String = "/Tiff_Show/Atlanta.tif"
+    //let stream: NSInputStream = ...;
+    //let data: Data = ...;
+    //let reader: TIFFByteReader = ...;
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("hello")  //mypngAtl
+        var filePath = Bundle.main.path(forResource: "Atl", ofType: "jpg")
+        //  var filePath = Bundle.main.path(forResource: "Atlanta", ofType: "tif")
+        guard let file = filePath else {return}
+        do {
+        let data = try Data(contentsOf: URL(fileURLWithPath: filePath!))
+           
+            let newView:UIImageView = UIImageView()
+            
+            guard let myimg = UIImage(data: data) else {return}
+          //  guard let myimg = UIImage(named: "Atl.jpg") else {return}
+           newView.image = myimg
+            newView.frame = CGRect(x: 0, y: 0, width: 2000, height: 2000)
+            newView.contentMode = .scaleAspectFit
+            newView.layer.cornerRadius = 10
+            newView.layer.borderColor = UIColor.black.cgColor
+            newView.layer.borderWidth = 5
+            
+//            let scView = UIScrollView()
+//            scView.contentSize = CGSize(width: 2000, height: 2000)
+//            scView.addSubview(newView)
+//
+            self.view.addSubview(newView)
+            
+          //  let imgdata:NSData = data as NSData
+//            guard let imgsource = CGImageSourceCreateWithData(imgdata, nil) else {return}
+//            let imageProperties = CGImageSourceCopyPropertiesAtIndex(imgsource, 0, nil) as! NSDictionary
+//                let pixelWidth = imageProperties[kCGImagePropertyPixelWidth] as! Int
+//                print("the image width is: \(pixelWidth)")
+//                let tiffDate = imageProperties[kCGImagePropertyTIFFDateTime] as! Date
+//            print(tiffDate.description)
+//
+            print("finished")
+        }catch{
+            print("file could not be loaded")
+        }
+        
     }
+//      //  var data     = Data(contentsOf: URL(fileURLWithPath: filePath!))
+//
+//
+//        // Do any additional setup after loading the view.
+//        let tiffImage: TIFFImage = TIFFReader.readTiff(fromFile: filePath);
+//        //let tiffImage: TIFFImage = TIFFReader.readTiff(from: stream);
+//        //let tiffImage: TIFFImage = TIFFReader.readTiff(from: data);
+//        //let v: TIFFImage = TIFFReader.readTiff(from: reader);
+//
+//
+//
+//        let directories: Array<TIFFFileDirectory> = tiffImage.fileDirectories();
+//      //  guard let first = directories[0] else {return}
+//        let directory: TIFFFileDirectory = directories[0]
+//        let rasters: TIFFRasters = directory.readInterleavedRasters()
+//        //let rasters: TIFFRasters = directory.readRasters();
+//        print("counting")
+//        print(rasters.size())
+//    }
 
 
 }
